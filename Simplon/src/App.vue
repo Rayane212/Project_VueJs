c0fe602033cda7d325bfac2e2b09332de3a190bc
Rayane Hadi <rayanehadi41@gmail.com>
2023-06-02 17:44:13 +0200

    first commit

---
+++ Simplon/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue'
+
+</script>
+
+<template>
+  <header>
+    <nav class="container-fluid">
+      <Navbar />
+    </nav>
+  </header>
+</template>
+
+<style scoped></style>
---
+++ Simplon/src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import IconFacebookVue from './icons/IconFacebook.vue';
+import IconProfileVue from './icons/IconProfile.vue';
+</script>
+<template>
+    <nav class="container-fluid">
+        <div class="container mt-1">
+            <a class="navbar-brand" href="#">
+                <IconFacebookVue />
+            </a>
+            <a>
+                <IconProfileVue />
+            </a>
+            <break />
+            <div class="d-flex w-50 position-absolute end-0">
+                <button class="btn btn-success me-2 " type="submit">ajouter ami / groupe </button>
+                <button class="btn btn-success" type="submit">Invitation en attente</button>
+            </div>
+        </div>
+
+
+    </nav>
+</template>
+
+<style></style>
